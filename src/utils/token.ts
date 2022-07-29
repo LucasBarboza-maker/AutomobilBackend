583947ababe58f2a0fc1254871bde8b4333cafed
LucasBarboza-maker <61573957+LucasBarboza-maker@users.noreply.github.com>
2022-07-29 17:00:38 -0300

    Chore: Add token handler

---
+++ src/utils/token.ts
@@ -0,0 +1,42 @@
+import * as jwt from 'jsonwebtoken';
+import {tokenInfo} from '@config/index' 
+import { IUser } from 'src/models/user';
+import { Response } from 'express';
+
+
+interface ICookieOptions {
+  expires: Date,
+  httpOnly: boolean,
+  secure?:boolean
+}
+
+const signToken = (id:string) => {
+  return jwt.sign({ id }, tokenInfo.JWT_SECRET, {
+      expiresIn: tokenInfo.JWT_EXPIRES_IN
+  });
+}
+
+const createSendToken = (user:IUser, statusCode:number, res:Response) => {
+  const token = signToken(user._id)
+
+  const cookieOptions : ICookieOptions = {
+      expires: new Date(Date.now() + tokenInfo.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+      httpOnly: true
+  };
+
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+  res.cookie('jwt', token, cookieOptions)
+
+  user.password = '';
+
+  res.status(statusCode).json({
+      status: 'success',
+      token,
+      data: {
+          user: user
+      }
+  })
+}
+
+export {createSendToken}
