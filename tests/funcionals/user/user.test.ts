3b382eef49d91579fda84bed6f57f72601edd147
LucasBarboza-maker <61573957+LucasBarboza-maker@users.noreply.github.com>
2022-07-30 00:34:28 -0300

    Test: test if register user inserts in database

---
+++ tests/funcionals/user/user.test.ts
@@ -0,0 +1,19 @@
+import UserService from '../../../src/services/userService'
+
+const user = {
+	name:"Lucas",
+	surname:"Rodrigues Barboza",
+	email:"lucasrb18@gmail.com",
+	phoneNumber:"5524993017636",
+	birth:new Date("1998-03-26"),
+	password:"test@123",
+	passwordConfirm:"test@123",
+	termsAndAgree:true
+}
+
+describe('User Service Tests', () => {
+  it('Register User account', async () => {
+    const result = await UserService.SignUp(user);
+    expect(result).toBeDefined()
+  });
+});
